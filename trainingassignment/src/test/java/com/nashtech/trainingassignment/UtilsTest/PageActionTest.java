c24101e33ae79cdf5cfb05d62d7d2808d108936b
Lương Quang Huy <lqhuy2309@gmail.com>
2021-10-06 09:59:00 +0700

    Unit test for PageAction

---
+++ trainingassignment/src/test/java/com/nashtech/trainingassignment/UtilsTest/PageActionTest.java
@@ -0,0 +1,18 @@
+package com.nashtech.trainingassignment.UtilsTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.nashtech.trainingassignment.utils.HttpRequest;
+import com.nashtech.trainingassignment.utils.PageAction;
+
+public class PageActionTest {
+	@Test
+	void getTotalPage_True() {
+		String response = "{\"message\": \"OK\", \"code\": 0, \"data\": {\"page_info\": "
+				+ "{\"total_number\": 43, \"page\": 1, \"page_size\": 10, \"total_page\": 5}, "
+				+ "\"list\": []}, \"request_id\": " + "\"2021100602400801024509900226A00E98\"}";
+		assertEquals(PageAction.getTotalPage(response), 5);
+	}
+}
